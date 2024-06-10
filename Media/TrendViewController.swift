d460078e569558ee20bc31c68a2a8267a240e9f6
NERO <mxzxro@gmail.com>
2024-06-11 01:00:24 +0900

    🔧 SceneDelegate Window RootVC 설정
    TrendViewController()

---
+++ Media/TrendViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TrendViewController.swift
+//  Media
+//
+//  Created by NERO on 6/10/24.
+//
+
+import UIKit
+
+class TrendViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}

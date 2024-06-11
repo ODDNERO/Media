d3a4a9110a0a47aabd5dd98c1d292eca2a407331
NERO <mxzxro@gmail.com>
2024-06-11 21:06:56 +0900

    💄 [SearchVC] CollectionView 설정 및 SearchCell 띄우기

--- Media/Search/SearchViewController.swift
+++ Media/Search/SearchViewController.swift
@@ -8,9 +8,64 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout())
+    
+    func collectionViewLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewFlowLayout()
+        let width = (UIScreen.main.bounds.width - 40)/3
 
+        layout.itemSize = CGSize(width: width, height: width*1.4)
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 10
+        layout.minimumInteritemSpacing = 10
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        
+        return layout
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureView()
+    }
+    
+    func configureView() {
+        view.backgroundColor = .white
+        collectionView.backgroundColor = .clear
+        view.addSubview(collectionView)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        collectionView.register(SearchCollectionViewCell.self, forCellWithReuseIdentifier: SearchCollectionViewCell.id)
+        
+        collectionView.snp.makeConstraints {
+            $0.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+}
+
+extension SearchViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100 //임시
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SearchCollectionViewCell.id, for: indexPath) as! SearchCollectionViewCell
+        return cell
+    }
+}
+
+class SearchCollectionViewCell: UICollectionViewCell {
+    static let id = "SearchCollectionViewCell"
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .black //임시
+        self.layer.cornerRadius = 10
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }

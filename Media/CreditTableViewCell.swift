1f3fd94be96a53b74b0aa22566727ca6223c4e0a
NERO <mxzxro@gmail.com>
2024-06-11 10:08:32 +0900

    ✨ CreditVC, CreditCell 파일 추가

---
+++ Media/CreditTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  CreditTableViewCell.swift
+//  Media
+//
+//  Created by NERO on 6/10/24.
+//
+
+import UIKit
+
+class CreditTableViewCell: UITableViewCell {
+    static let identifier = "CreditTableViewCell"
+
+
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Media/CreditViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CreditViewController.swift
+//  Media
+//
+//  Created by NERO on 6/10/24.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemGray4 //임시
+    }
+}

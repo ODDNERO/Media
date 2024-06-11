00d1b51787b84af53b5525a091cbd0fcf74372b0
NERO <mxzxro@gmail.com>
2024-06-11 10:40:55 +0900

    ✨ [TrendVC] TableView 구현, Cell에 값 전달

--- Media/TrendViewController.swift
+++ Media/TrendViewController.swift
@@ -40,17 +40,44 @@ extension TrendViewController {
     }
     
     func configureData() {
+        movieTableView.delegate = self
+        movieTableView.dataSource = self
         movieTableView.register(TrendTableViewCell.self, forCellReuseIdentifier: TrendTableViewCell.identifier)
+    }
+}
+
+extension TrendViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movieList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: TrendTableViewCell.identifier, for: indexPath) as! TrendTableViewCell
+        cell.movieID = movieList[indexPath.row].id
+        cell.movieTitleLabel.text = movieList[indexPath.row].title
+        cell.releaseDateLabel.text = movieList[indexPath.row].release_date
+        cell.overviewLabel.text = movieList[indexPath.row].overview
+        cell.voteAverageLabel.text = "\(movieList[indexPath.row].vote_average)"
+        //        cell.posterImageView.image
+        //        cell.backdropImageView.image
+        cell.backgroundColor = .clear
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 300 //임시
     }
 }
 
 //MARK: - Configure UI
 extension TrendViewController {
     func configureLayout() {
+        movieTableView.snp.makeConstraints { $0.edges.equalTo(view.safeAreaLayoutGuide) }
     }
     
     func configureUI() {
         view.backgroundColor = .white
+        movieTableView.separatorStyle = .none
     }
 }
 
